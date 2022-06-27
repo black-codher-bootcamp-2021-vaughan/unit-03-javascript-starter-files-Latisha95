71b8f71421c419bfc613c5fe75a151071236f222
Latisha <latisha@LAPTOP-ALEXC.localdomain>
2022-06-28 00:38:06 +0100

    session-2 Tasks

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,5 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+let html = document.getElementById('h1').innerHTML;
+console.log(document.querySelector('#h1').innerHTML);
+console.log('abouts:', document.querySelectorAll('about'))
