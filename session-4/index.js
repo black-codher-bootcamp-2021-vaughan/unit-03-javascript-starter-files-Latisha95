45e2a979f19d8e32615b1adc9042581af4640974
Latisha <latisha@LAPTOP-ALEXC.localdomain>
2022-07-02 13:19:06 +0100

    Session-04 Task-1-Objects

--- session-4/index.js
+++ session-4/index.js
@@ -1,11 +1,84 @@
 // ****************************************************TASK 1 - OBJECTS****************************************************
+const personA = {
+    name: 'Jane',
+    location: 'Kent',
+    age: 32,
+    likes: 'baking'
+}
 
+const personB = {
+    name: 'Taylor',
+    location: 'Scotland',
+    age: 22,
+    likes: 'rock climbing'
+}
 
+const personC = {
+    name: 'Martin',
+    location: 'Birmingham',
+    age: 41,
+    likes: 'gardening'
+}
 
+const personD = {
+    name: 'Lisa',
+    location: 'Manchester',
+    age: 21,
+    likes: 'shopping'
+}
 
+console.log(personA.name);
 
+function biography(person) {
 
+    return "Hi, my name is " + person.name + " I am " + person.age + " years old, " + " i live in " + person.location " and i like " + person.likes
+};
 
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD));
+
+//Helene's Example
+
+const personA = {
+    name: 'Ahoua',
+    age: 25,
+    location: 'Coventry',
+    likes: 'decorating',
+ };
+ const personB = {
+     name: 'Laura',
+     age: 32,
+     location: 'France',
+     likes: 'painting',
+  };
+  const personC = {
+     name: 'Harold',
+     age: 22,
+     location: 'Paris',
+     likes: 'playing football',
+  };
+  const personD = {
+     name: 'Karl',
+     age: 52,
+     location: 'London',
+     likes: 'running',
+  };
+  function biography (person) {
+ // 1st way :
+ const {name, age, likes, location} = person
+ //2nd way :
+     // const name = person.name;
+    // const age = person.age;
+    // const location = person.location;
+    // const likes = person.likes;
+     return (`Hi, my name is ${name}. I am ${age} years old, I live in ${location} and I like ${likes}.`);
+  }
+ console.log(biography(personA))
+ console.log(biography(personB))
+ console.log(biography(personC))
+ console.log(biography(personD))
 
 
 
@@ -19,50 +92,99 @@
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
 
-// const x = 6;
-// const y = 4;
+ const x = 6;
+ const y = 4;
 
-// const addition = x + y;
+ const addition = x + y;
 
-// console.log("Addition: x + y " + addition);
+ console.log("Addition: x + y " + addition);
 
-// const subtraction = x - y;
+ const subtraction = x - y;
 
-// console.log("Subtraction: x - y " + subtraction);
+ console.log("Subtraction: x - y " + subtraction);
 
-// const multiplication = x * y;
+ const multiplication = x * y;
 
-// console.log("multiplication: x * y " + multiplication);
+ console.log("multiplication: x * y " + multiplication);
 
-// const division = x * y;
+ const division = x / y;
 
-// console.log("Division: x + y " + division);
+ console.log("Division: x / y " + division);
 
+ const multiplication = x * x;
 
+ console.log("multiplication: x * x " + multiplication);
 
+ const addition = x + x + y;
 
+ console.log("addition: x + x + y " + addition);
 
+ const sum = x * y / x;
 
-
-
-
-
-
+ console.log("sum: x * y / x " + sum);
 
 
 
 
 // ****************************************************TASK 3****************************************************
+const age = 24
+const minimumAge = 18
+
+ const isOld = age >= minimumAge
+
+ console.log('Are you old enough? ' + isOld)
 
 
+ //*************************************************Homework Calculator********************************************
 
+ function add (x,y) {
+     var z = x + y;
+     return x + " + " + y + " = " + z;
+ }
 
+ // console.log(add (5,2));
 
+ function sub(x, y) {
+     var z = x - y;
+     return x + " - " + y + " = " + z;   
+ }
 
+ // console.log(sub (4,2));
 
+ function multi(x, y) {
+     var z = x * y;
+     return x + " * " + y + " = " + z;
+ }
 
+// console.log(multi (6,5));
 
+function div(x, y){
+    var z = x / y;
+    return x + " / " + y + " = " + z;
+ }
 
+ //console.log(div(7,9));
+ 
+ const calculator = {
+     addition: add,
+     subtraction: sub,
+     multiplication: multi,
+     division: div,
+}
+
+console.log(calculator.addition(3,5))
+console.log(calculator.subtraction(8,5))
+console.log(calculator.multiplication(10,5))
+console.log(calculator.division(8,4))
+
+//*******************************************************Homework Task 2*********************************************
+
+function whoIsOlder(person1, person2) {
+    var age = person1.age > person2.age;
+    var ageDifference = person1.age - person2.age;
+    return "Is " + person1.name + " older than " + person2.name + "? " + age + "." + " The difference of age is " +
+}
+console.log(whoIsOlder(personB, personC))
 
 
 
