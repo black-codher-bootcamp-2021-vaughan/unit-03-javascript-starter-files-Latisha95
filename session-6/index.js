c9108d3bffa76dab577cd7f9b957e26fe5e52aaf
Latisha <latisha@LAPTOP-ALEXC.localdomain>
2022-07-06 00:33:51 +0100

    Task-1 Task-2

--- session-6/index.js
+++ session-6/index.js
@@ -1,9 +1,17 @@
 // Use this randomNumber inside of the while/for loops to generate a random number
-const randomNumber = Math.round(Math.random() * 10);
+//const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 1****************************************************
+let count = 0
+while (count <= 10) {
+    let randomNumber = Math.round(Math.random() * 10);
+    count = count + 1;
 
+    console.log('In the While ' + randomNumber);
 
+}
+
+console.log('Random number is: ' + randomNumber);
 
 
 
@@ -19,11 +27,15 @@ const randomNumber = Math.round(Math.random() * 10);
 
 // ****************************************************TASK 2****************************************************
 
+let count = 0;
+let randomNumber = Math.round(Math.random() * 10);
 
+for (let count; count < 10; count++) {
+    randomNumber = randomNumber + 1
+    count = count + 1;
+}
 
-
-
-
+console.log('For was here ' + randomNumber );
 
 
 
