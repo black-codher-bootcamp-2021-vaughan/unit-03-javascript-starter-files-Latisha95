23ed5c1f92899e7af3e9c767c8a4ee905302fff2
Latisha <latisha@LAPTOP-ALEXC.localdomain>
2022-07-08 23:48:27 +0100

    All session Tasks

--- session-7/index.js
+++ session-7/index.js
@@ -1,5 +1,8 @@
 // ****************************************************TASK 1****************************************************
+const animals = ['cat', 'dog', 'wolf', 'lion', 'eagle', 'zebra'];
+animals.pop();
 
+console.log(animal);
 
 
 
@@ -16,7 +19,29 @@
 
 // ****************************************************TASK 2****************************************************
 
+const animals = ['cat', 'dog', 'wolf', 'lion', 'eagle', 'zebra'];
 
+animals.sort();
+
+animals.reverse();
+
+console.log(animal);
+
+const names = ['Sarah', 'Jane', 'Gemma', 'josh', 'Aisha', 'Danait', 'Luke', 'Connor', 'Alaia', 'Cieran'];
+
+names.sort();
+
+names.reverse();
+
+console.log(names);
+
+//bonus
+
+const numbers = ['5', '325', '67', '100000', '150'];
+
+numbers.sort();
+
+console.log(numbers);
 
 
 
@@ -35,8 +60,21 @@ const people = [
   { name: 'killmonger', age: 32 },
 ];
 
+
 // This is a helper function that should be used in the final task of the session.
 // You should pass this function to the sort method to sort the ages of each individiual in the people array of objects
-function compare(a, b) {
+//function compare(a, b) {
+  //return a.age - b.age;
+//}
+
+people.sort((a, b) => {
   return a.age - b.age;
 }
+);
+let names = [];
+
+for (let index = 0; index < people.length; index++) {
+  names.push(people[index].name)
+}
+
+console.log(names);
